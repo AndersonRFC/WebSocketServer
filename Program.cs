b54d254717532d1e0dcf541c3c4eba71eada1ef4
Anderson Gomes <anderson.informatica2016@gmail.com>
2024-04-08 00:02:08 -0300

    Po, sem comentarios kkk. Versao estavel WSF

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+var clientesConectados = new List<WebSocket>();
+
+var usersConectados = new List<User>();
+
+var mensagensPreCriadas = new List<string>
+{
+	"Bem-vindo ao chat!",
+	"O servidor esta pronto para receber suas mensagens."
+};
+
 app.UseWebSockets();
 
 app.MapGet("/", () => "Hello World!");
@@ -17,7 +27,37 @@ app.Map("/ws", async context =>
 		var webSocket = await context.WebSockets.AcceptWebSocketAsync();
 		await Console.Out.WriteLineAsync("Recebemos uma conexão Websocket!");
 
-		await ReceberMensagens(webSocket);
+		clientesConectados.Add(webSocket);
+
+		var newUser = new User
+		{
+			Connection = webSocket,
+			Name = webSocket.GetHashCode().ToString(),
+		};
+
+		usersConectados.Add(newUser);
+
+		await NotificarEntrada(newUser);
+
+		await AtualizarListaUsuarios(usersConectados);
+
+		foreach (var mensagem in mensagensPreCriadas)
+		{
+			Mensagem mensagemResponse = new Mensagem()
+			{
+				Type = "text",
+				Name = "System",
+				Data = mensagem
+			};
+
+			var jsonResponse = JsonConvert.SerializeObject(mensagemResponse);
+
+			var bufferMensagem = Encoding.UTF8.GetBytes(jsonResponse);
+
+			await webSocket.SendAsync(new ArraySegment<byte>(bufferMensagem), WebSocketMessageType.Text, true, CancellationToken.None);
+		}
+
+		await ReceberMensagens(webSocket, newUser);
 	}
 	else
 	{
@@ -25,7 +65,7 @@ app.Map("/ws", async context =>
 	}
 });
 
-async Task ReceberMensagens(WebSocket webSocket)
+async Task ReceberMensagens(WebSocket webSocket, User user)
 {
 	var buffer = new byte[10 * 1024 * 1024];
 
@@ -39,7 +79,9 @@ async Task ReceberMensagens(WebSocket webSocket)
 
 	bool trintaFlag = true;
 
-	bool sessentaflag = true;
+	bool sessentaFlag = true;
+
+	bool noventaNoveFlag = true;
 
 	bool canprever = false;
 
@@ -57,13 +99,21 @@ async Task ReceberMensagens(WebSocket webSocket)
 			Console.WriteLine("trinta flag -----\n\n");
 
 			trintaFlag = false;
-			await EnviarMensagem("30", "info", webSocket);
+			await EnviarMensagem("30", "info", webSocket, user, false);
 		}
-		else if (sumResultado > (previsto / 1.5) && canprever && sessentaflag)
+		else if (sumResultado > (previsto / 1.5) && canprever && sessentaFlag)
 		{
 			Console.WriteLine("sessenta flag -----\n\n");
-			sessentaflag = false;
-			await EnviarMensagem("60", "info", webSocket);
+			sessentaFlag = false;
+			await EnviarMensagem("60", "info", webSocket, user, false);
+		}
+		else if(sumResultado >= (previsto / 1.1) && canprever && noventaNoveFlag)
+		{
+			Console.WriteLine("noventaNove flag -----\n\n");
+
+			noventaNoveFlag = false;
+
+			await EnviarMensagem("99", "info", webSocket, user, false);
 		}
 
 
@@ -83,7 +133,7 @@ async Task ReceberMensagens(WebSocket webSocket)
 				if (_mensagem.Type == "text")
 				{
 					await Console.Out.WriteLineAsync(_mensagem.Name + ": " + _mensagem.Data);
-					await EnviarMensagem(_mensagem.Data, "text", webSocket);
+					await EnviarMensagem(_mensagem.Data, "text", webSocket, user, true);
 					canprever = false;
 
 				}
@@ -102,7 +152,7 @@ async Task ReceberMensagens(WebSocket webSocket)
 
 						await Console.Out.WriteLineAsync("Arquivo salvo com sucesso em: " + caminhoCompleto);
 
-						await EnviarMensagem("documents/" + _mensagem.Name, "file", webSocket);
+						await EnviarMensagem("documents/" + _mensagem.Name, "file", webSocket, user, true);
 
 					}
 					catch (Exception ex)
@@ -123,14 +173,21 @@ async Task ReceberMensagens(WebSocket webSocket)
 					}
 					catch
 					{
-                        await Console.Out.WriteLineAsync("\n\nMensagem Data: " + mensagem);
-                    }
+						await Console.Out.WriteLineAsync("\n\nMensagem Data: " + mensagem);
+					}
 				}
+				else if (_mensagem.Type == "rename")
+				{
+					user.Name = _mensagem.Data;
+					await AtualizarListaUsuarios(usersConectados);
+				}
+
 				count = 0;
 				sumResultado = 0;
 				mensagem = string.Empty;
 				trintaFlag = true;
-				sessentaflag = true;
+				sessentaFlag = true;
+				noventaNoveFlag = true;
 			}
 
 		}
@@ -140,14 +197,22 @@ async Task ReceberMensagens(WebSocket webSocket)
 			break;
 		}
 	}
+
+	//var thisUser = usersConectados.Where(u => u.Connection == webSocket).ToList().First();
+
+	usersConectados.Remove(user);
+	clientesConectados.Remove(webSocket);
+	await NotificarSaida(user);
+	await AtualizarListaUsuarios(usersConectados);
+
 }
 
-async Task EnviarMensagem(string mensagem, string tipo, WebSocket webSocket)
+async Task EnviarMensagem(string mensagem, string tipo, WebSocket webSocket, User user, bool toAll)
 {
 	Mensagem mensagemResponse = new Mensagem()
 	{
 		Type = tipo,
-		Name = "Você",
+		Name = user.Name,
 		Data = mensagem,
 		Size = mensagem.Length
 	};
@@ -155,8 +220,98 @@ async Task EnviarMensagem(string mensagem, string tipo, WebSocket webSocket)
 	var jsonResponse = JsonConvert.SerializeObject(mensagemResponse);
 
 	var bufferMensagem = Encoding.UTF8.GetBytes(jsonResponse);
+	if (toAll)
+	{
+		foreach (var cliente in clientesConectados)
+		{
+			if (cliente.State == WebSocketState.Open)
+			{
+				await cliente.SendAsync(new ArraySegment<byte>(bufferMensagem), WebSocketMessageType.Text, true, CancellationToken.None);
+			}
+		}
+	}
+	else
+	{
+		await webSocket.SendAsync(new ArraySegment<byte>(bufferMensagem), WebSocketMessageType.Text, true, CancellationToken.None);
+	}
+}
 
-	await webSocket.SendAsync(new ArraySegment<byte>(bufferMensagem), WebSocketMessageType.Text, true, CancellationToken.None);
+async Task NotificarEntrada(User user)
+{
+	Mensagem mensagemResponse = new Mensagem()
+	{
+		Type = "newUser",
+		Name = user.Name,
+		Data = user.Name,
+		Size = user.Name.Length
+	};
+
+	var jsonResponse = JsonConvert.SerializeObject(mensagemResponse);
+
+	var bufferMensagem = Encoding.UTF8.GetBytes(jsonResponse);
+
+	foreach (var cliente in clientesConectados)
+	{
+		if (cliente.State == WebSocketState.Open)
+		{
+			await cliente.SendAsync(new ArraySegment<byte>(bufferMensagem), WebSocketMessageType.Text, true, CancellationToken.None);
+		}
+	}
+}
+
+async Task NotificarSaida(User user)
+{
+	Mensagem mensagemResponse = new Mensagem()
+	{
+		Type = "outUser",
+		Name = user.Name,
+		Data = user.Name,
+		Size = user.Name.Length
+	};
+
+	var jsonResponse = JsonConvert.SerializeObject(mensagemResponse);
+
+	var bufferMensagem = Encoding.UTF8.GetBytes(jsonResponse);
+
+	foreach (var cliente in clientesConectados)
+	{
+		if (cliente.State == WebSocketState.Open)
+		{
+			await cliente.SendAsync(new ArraySegment<byte>(bufferMensagem), WebSocketMessageType.Text, true, CancellationToken.None);
+		}
+	}
+}
+
+async Task AtualizarListaUsuarios(List<User> usuariosConectados)
+{
+	List<UserResponse> usersResponse = new();
+
+	foreach (var user in usuariosConectados)
+	{
+		usersResponse.Add(new UserResponse
+		{
+			Name = user.Name,
+		});
+	}
+
+	var ListaAtualizada = new
+	{
+		Type = "listUsers",
+		Data = usersResponse.ToArray()
+	};
+
+	var jsonResponse = JsonConvert.SerializeObject(ListaAtualizada);
+
+	var bufferMensagem = Encoding.UTF8.GetBytes(jsonResponse);
+
+	foreach (var cliente in clientesConectados)
+	{
+		if (cliente.State == WebSocketState.Open)
+		{
+			await cliente.SendAsync(new ArraySegment<byte>(bufferMensagem), WebSocketMessageType.Text, true, CancellationToken.None);
+		}
+	}
+
 }
 
 app.Run();
@@ -167,4 +322,15 @@ public class Mensagem
 	public string Name { get; set; }
 	public string Data { get; set; }
 	public int Size { get; set; }
+}
+
+public class User
+{
+	public WebSocket Connection { get; set; }
+	public string Name { get; set; }
+}
+
+public class UserResponse
+{
+	public string Name { get; set; }
 }
